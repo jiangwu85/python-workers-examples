42dd62066236b3eac902e1cb9b6b35281051e888
Wu Jiang <Wu_Jiang@swissre.com>
2025-12-02 18:18:49 +0800

    init01

--- 04-query-d1/src/entry.py
+++ 04-query-d1/src/entry.py
@@ -3,6 +3,7 @@ from workers import WorkerEntrypoint, Response
 
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
+        import asgi
         query = """
         SELECT quote, author
         FROM qtable
@@ -11,6 +12,6 @@ class Default(WorkerEntrypoint):
         """
         results = await self.env.DB.prepare(query).all()
         data = results.results[0]
-
         # Return a JSON response
-        return Response.json(data)
+        print( Response.json(data))
+        return await asgi.fetch(app, request.js_object, self.env)
